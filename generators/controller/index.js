4bc82d85dcfcc247100930b2651de1788edc8c46
Baptiste Lemoine <baptiste.lemoine@cofelyineo-gdfsuez.com>
2015-09-11 11:28:11 +0200

    adding a controller generator

---
+++ generators/controller/index.js
@@ -0,0 +1,62 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+    initializing: function () {
+        this.pkg = require('../../package.json');
+
+    },
+
+    prompting: function () {
+        var done = this.async();
+
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the supreme ' + chalk.red('AngularJeej') + ' generator! Aint nobody got time for that?'
+        ));
+        this.log(chalk.green('let\'s build dat controller'));
+        var prompts = [
+            {
+                type: 'input',
+                name: 'app_name',
+                message: 'name of your angular app?',
+                default: 'myApp',
+                store: true
+            },
+            {
+                type: 'input',
+                name: 'ctrl_name',
+                message: 'name of your new controller? it should be unique',
+                default: 'myController',
+                store: true
+            }];
+
+        this.prompt(prompts, function (props) {
+            this.props = props;
+            // To access props later use this.props.someOption;
+
+            done();
+        }.bind(this));
+    },
+
+    writing: {
+        app: function () {
+            this.fs.copyTpl(
+                this.templatePath('newCtrl.coffee'),
+                this.destinationPath('src/coffee/'+this.props.ctrl_name+'.coffee'),
+                {
+                    app_name: this.props.app_name,
+                    ctrl_name: this.props.ctrl_name
+                }
+            );
+        }
+    },
+
+    install: function () {
+        //this.installDependencies();
+        this.log(chalk.green('wala! enjoy your new controller!'));
+        this.log(chalk.green('Dont forget to include your controller in your app\'s template!'));
+    }
+});
